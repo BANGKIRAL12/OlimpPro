18ea4c8b0c26b3e41147e8c3a5c52322c0c4b0d9
cahyakiral <cahyakiral12@gmail.com>
2026-05-02 16:07:12 +0700

    edit lagi.

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import {
   Calculator, Pi, FunctionSquare, LayoutGrid, Gem, Crown, Flame, Microscope, 
   Globe2, BookOpen, Clock, Sparkles, Pencil, MessageSquare, X, Eraser, Trash2, Send
 } from 'lucide-react';
-import soal from './database.json'
+import data from './database.json'
 
 const apiKey = ""; 
 
 // Database Bidang per Mapel
-const MAPEL_CONFIG = soal || {
+const MAPEL_CONFIG = data.soal || {
   matematika: ['Campuran', 'Aljabar', 'Geometri', 'Teori Bilangan', 'Kombinatorika'],
   ipa: ['Campuran', 'Biologi', 'Fisika', 'Kimia', 'Astronomi', 'Kebumian'],
   ips: ['Campuran', 'Geografi', 'Ekonomi', 'Sejarah', 'Sosiologi']
